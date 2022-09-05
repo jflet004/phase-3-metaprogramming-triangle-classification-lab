048c48271417adc8267a4e6211246f0817f316e1
Jose Fletes <joseangelfletes@gmail.com>
2022-09-05 15:32:51 -0700

    commit tree c1e56e79b371b008eebb5f41f58d733c5a3138fa

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
+require "pry"
+
 class Triangle
-  # write code here
+
+  attr_reader :l1, :l2, :l3
+
+  def initialize (l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+  
+  def kind
+
+    triangle_validation
+
+    if l1 == l2 && l2 == l3
+      :equilateral
+    elsif l1 == l2 || l2 == l3 || l1 == l3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  def triangle_validation
+    raise TriangleError unless no_size? && triangle_inequality?
+  end
+
+  def no_size?
+    [l1, l2, l3].all?{|length| length.positive?}
+  end
+
+  def triangle_inequality?
+    l1 + l2 > l3 && l2 + l3 > l1 && l1 + l3 > l2
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
